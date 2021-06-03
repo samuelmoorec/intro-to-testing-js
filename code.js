b78cb448bedc107aea61a31a332b479882713423
Samuel Moore <samuel.c.moore1221@gmail.com>
2021-06-03 10:56:55 -0500

    added second way of writing function

--- code.js
+++ code.js
@@ -5,15 +5,24 @@ function helloWorld() {
 
 function sayHello(input){
 
-
-
     if (typeof input === "string" && isNaN(input)) {
             return "Hello, " + input + "!"
 
     }
+
     return "Hello, World!";
-
-
-
 }
+
+
+// function sayHello(input){
+//
+//     if (typeof input === "string") {
+//
+//         if (isNaN(input)){
+//             return "Hello, " + input + "!"
+//         }
+//     }
+//
+//     return "Hello, World!";
+// }
 
