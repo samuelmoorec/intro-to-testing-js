7827e9e19d257fb03d646c68a0553a4fc155c3ad
Samuel Moore <samuel.c.moore1221@gmail.com>
2021-06-03 09:51:21 -0500

    commiting on exercise #7

--- code.js
+++ code.js
@@ -3,5 +3,7 @@ function helloWorld() {
     return "Hello, World!";
 }
 
-function sayHello(){
+function sayHello(input){
+    return "Hello, " + input + "!"
 }
+
--- tests.js
+++ tests.js
@@ -18,7 +18,26 @@ describe("sayHello", function (){
     it('should be a defined function.', function () {
         expect(typeof sayHello).toBe('function');
     });
+    it('should return a string when called', function () {
+        expect(typeof sayHello()).toBe("string");
+    });
+    it('should return "Hello, Jane!" when passed the argument "Jane".', function () {
+        expect(sayHello("Jane")).toBe("Hello, Jane!");
+    });
+    it('should return "Hello, Alex!" when passed the argument "Alex".', function () {
+        expect(sayHello("Alex")).toBe("Hello, Alex!");
+    });
+    it('should return "Hello, Pat!" when passed the argument "Pat".', function () {
+        expect(sayHello("Pat")).toBe("Hello, Pat!");
+    });
+    // it('should return "Hello, name!" when passed the argument representing someones name.', function () {
+    //     expect(sayHello("Jane")).toBe("Hello, Jane!");
+    //     expect(sayHello("Pat")).toBe("Hello, Pat!");
+    //     expect(sayHello("Alex")).toBe("Hello, Alex!");
+    // });
+
 })
+
 
 
 
